d350e1cc91d69ea1615fe66bdca4ac522b2ed64f
Weber Chu <weberchu@amazon.com>
2023-07-19 12:03:49 +0100

    Generate HTMl with Handlebars

--- src/handlers/price-generator.mjs
+++ src/handlers/price-generator.mjs
@@ -1,14 +1,18 @@
 import AWS from 'aws-sdk';
-const s3 = new AWS.S3();
+import fs from 'fs';
+import Handlebars from "handlebars";
 
+const s3 = new AWS.S3();
 const key = `price.html`;
 const bucketName = 'octopus-tracker';
 
-async function uploadToS3(fileContent) {
+const uploadToS3 = async (fileContent) => {
     const params = {
         Bucket: bucketName,
         Key: key,
         Body: fileContent,
+        CacheControl: "max-age=10",
+        ContentType: "text/html"
     };
 
     try {
@@ -20,9 +24,33 @@ async function uploadToS3(fileContent) {
     }
 }
 
-export const handler = async (event, context) => {
-    console.info(JSON.stringify(event));
+const getPrices = () => {
+    return {
+        prices: [
+            {
+                date: "7/19",
+                electricityPrice: "12.34p",
+                gasPrice: "12.34p",
+            },
+            {
+                date: "7/20",
+                electricityPrice: "14.34p",
+                gasPrice: "14.34p",
+            }
+        ],
+        lastUpdateTime: new Date().toLocaleString('en-GB', { timeZone: 'Europe/London' })
+    };
+}
 
-    const fileContent = 'Hello, S3!';
-    return await uploadToS3(fileContent);
+const generateHtml = (prices) => {
+    const template = fs.readFileSync("./src/template/price.hbs", {encoding: "utf8", flag: "r"});
+    const compiledTemplate = Handlebars.compile(template);
+    return compiledTemplate(prices);
+}
+
+export const handler = async (event, context) => {
+    const prices = getPrices();
+    const htmlContent = generateHtml(prices);
+    console.info("htmlContent = " + htmlContent);
+    return await uploadToS3(htmlContent);
 }
