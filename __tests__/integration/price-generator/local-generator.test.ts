9aa04600b2459b5275a55be40b6d66d0e9987373
Weber Chu <weberchu@amazon.com>
2023-11-04 21:49:36 +0000

    Add links to other regions

---
+++ __tests__/integration/price-generator/local-generator.test.ts
@@ -0,0 +1,38 @@
+import { Region } from "../../../src/price-generator/types";
+import { generateHtml } from "../../../src/price-generator/html-generator";
+import fs from "fs";
+
+const generate = async (region: Region) => {
+    const generationTimeStamp = new Date();
+
+    const prices = [
+        {
+            date: "10/07",
+            electricityPrice: "21.10",
+            gasPrice: "2.04",
+        },
+        {
+            date: "11/07",
+            electricityPrice: "23.09",
+            gasPrice: "4.05",
+        },
+        {
+            date: "12/07",
+            electricityPrice: "10.20",
+        },
+    ];
+
+    const htmlContent = generateHtml(region, prices, generationTimeStamp);
+
+    fs.writeFileSync("./" + region.pageNames[0], htmlContent);
+}
+
+/**
+ * Not a test. Run this to generate an HTML for manual verification.
+ */
+describe.skip("local-generator", () => {
+    test("generate", async () => {
+        await generate(Region.London);
+    });
+});
+
--- src/price-generator/html-generator.ts
+++ src/price-generator/html-generator.ts
@@ -4,20 +4,34 @@ import templateLocation from "../template/price.hbs";
 import Handlebars from "handlebars";
 import { toLastUpdateTime } from "./date-time-formatter";
 
+type RegionLink = {
+    regionName: string,
+    link: string,
+}
+
 type templateContext = {
     regionName: string,
     prices: Price[],
     lastUpdateTime: string,
+    otherRegions: RegionLink[]
 }
 
 export const generateHtml = (region: Region, prices: Price[], generationTimestamp: Date) => {
     const template = fs.readFileSync(templateLocation, {encoding: "utf8", flag: "r"});
     const compiledTemplate = Handlebars.compile(template);
+    const otherRegionLinks =
+        Region.ALL
+            .filter(r => r !== region)
+            .map(r => ({
+                regionName: r.name,
+                link: "./" + r.pageNames[0]
+            }));
 
     const context: templateContext = {
-        regionName: "blah",
+        regionName: region.name,
         prices,
-        lastUpdateTime: toLastUpdateTime(generationTimestamp)
+        lastUpdateTime: toLastUpdateTime(generationTimestamp),
+        otherRegions: otherRegionLinks
     }
 
     return compiledTemplate(context);
